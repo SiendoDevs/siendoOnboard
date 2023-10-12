e4a40e0e62a29798d29527bd7b942b6822eb0681
Cato <catriel.sl@gmail.com>
2023-10-11 23:53:17 -0300

    Pantalla 1

---
+++ src/components/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../src/img/logo-siendo.png';
+
+function Logo() {
+  return (
+    <div className="flex items-center justify-center"> {/* Agrega las clases para centrar */}
+      <Image
+        src={logo}
+        alt="Siendo Logo"
+        width={150} // Ancho deseado en píxeles
+        height={50} // Alto deseado en píxeles
+      />
+    </div>
+  );
+}
+
+export default Logo;
