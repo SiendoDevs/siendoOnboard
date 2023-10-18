b322cd9d808a88da242962d28bbb193be256d927
Cato <catriel.sl@gmail.com>
2023-10-18 19:25:29 -0300

    back to js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import BlackButton from '@/components/BlackButton';
 import Link from 'next/link';
 import Logo from '@/components/Logo';
 
-
 function Home() {
   return (
     <div className="bg-gradient-to-b from-[#52ffd1] via-[#ff20f8] to-[#ff20f8] dark:from-[#99ee99] dark:via-[#52ffd1] dark:to-[#ff20f8] min-h-screen flex flex-col items-center justify-center">
@@ -11,7 +10,6 @@ function Home() {
       <Link href="/home">
         <BlackButton text="COMENZAR" />
       </Link>
-      
     </div>
   );
 }
